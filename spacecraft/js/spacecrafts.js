b51f2c8ec236c59d29bf28fe1cd5a4e61a86226c
José Ramírez <jose.ramirez@thinksmartone.com>
2018-03-20 18:01:37 +0100

    Últimos cambios.

--- spacecraft/js/spacecrafts.js
+++ spacecraft/js/spacecrafts.js
@@ -47,10 +47,8 @@ $(function()
 			    dataType: 'JSON',
 			    data: {name: name, type:type, x:x, y:y, z:z},
 			    success: function(result) {
-			        console.log(result);
 			        if (result.error==0){
 			        	bootbox.alert("Nave registrada", function(){ location.href="spaceships_manager.php?accion=enter"; });
-			        	
 			        }
 			        else
 			        	bootbox.alert("No se ha podido registrar");
@@ -59,7 +57,6 @@ $(function()
 			    error: function(result) {
 			        // Control de errores
 			        console.log('Error');
-			        console.log(result);
 			        bootbox.alert("Se ha producido un error en el sistema");
 			    }
 			});
@@ -68,45 +65,49 @@ $(function()
 	
 	$('#btnSearch').click(function(){
 		var form = $('#spaceshipForm');
-		$.ajax({
-		    url: "http://localhost/api2/api/public/api/v1/ship/"+$('input[name=search]').val(),
-		    type: 'GET',
-		    dataType: 'JSON',
-		    success: function(result) {
-		        console.log(result);
-		        if (result.length>0){
-		        	let modal = $('#modalCoordenadas');
-		        	let naves = result.length;
-		        	let texto;
-		        	let body = modal.find(".modal-body");
-		        	body.empty();
-		        	for (i=0;i<naves;i++){
-		        		texto = '';
-		        		texto+="<h4>Nave espacial: " + result[i].name + "</h4>";
-		        		texto+="<p>Coordenada x: " + result[i].x + "<p>";
-		        		texto+="<p>Coordenada y: " + result[i].y + "<p>";
-		        		texto+="<p>Coordenada z: " + result[i].z + "<p>";
-		        		body.append(texto);
-		        	}
-		        	$('#modalCoordenadas').modal();
-		        }
-		        else{
-		        	bootbox.alert("No se encuentra en la flota rebelde");
-		        }
-		        
-		    },
-		    error: function(result) {
-		        // Control de errores
-		        console.log('Error');
-		        console.log(result);
-		        bootbox.alert("Se ha producido un error en el sistema");
-		    }
-		});
+		var search = $('input[name=search]').val();
+		if (search=='')
+		{
+			bootbox.alert("Debe introducir el nombre para buscar");
+		}
+		else{
+			$.ajax({
+			    url: "http://localhost/api2/api/public/api/v1/ship/"+$('input[name=search]').val(),
+			    type: 'GET',
+			    dataType: 'JSON',
+			    success: function(result) {
+			        if (result.length>0){
+			        	let modal = $('#modalCoordenadas');
+			        	let naves = result.length;
+			        	let texto;
+			        	let body = modal.find(".modal-body");
+			        	body.empty();
+			        	for (i=0;i<naves;i++){
+			        		texto = '';
+			        		texto+="<h4>Nave espacial: " + result[i].name + "</h4>";
+			        		texto+="<p>Coordenada x: " + result[i].x + "<p>";
+			        		texto+="<p>Coordenada y: " + result[i].y + "<p>";
+			        		texto+="<p>Coordenada z: " + result[i].z + "<p>";
+			        		body.append(texto);
+			        	}
+			        	$('#modalCoordenadas').modal();
+			        }
+			        else{
+			        	bootbox.alert("No se encuentra en la flota rebelde");
+			        }
+			        
+			    },
+			    error: function(result) {
+			        // Control de errores
+			        console.log('Error');
+			        bootbox.alert("Se ha producido un error en el sistema");
+			    }
+			});
+		}
 	});
 	
 	
 	$('input[name=x],input[name=y],input[name=z]').keydown(function(e){
-		console.log(e.keyCode);
 		if (e.keyCode!=46&&e.keyCode!=8&&e.keyCode!=37&&e.keyCode!=39&&e.keyCode!=9&&e.keyCode!=109)
 		{
 			// Si el valor no es numérico, lo borramos
@@ -121,5 +122,14 @@ $(function()
 		form.find("input[name=x]").val('');
 		form.find("input[name=y]").val('');
 		form.find("input[name=z]").val('');
+	});
+	
+	$('#btnExit').click(function(){
+		bootbox.confirm("¿Desea salir del sistema?", 
+			function(result){
+				if (result)
+					location.href="index.html"; 
+			}
+		);
 	});
 });
--- spacecraft/spaceships_manager.php
+++ spacecraft/spaceships_manager.php
@@ -1,4 +1,5 @@
 <?php
+
 // Inclusión de TWIG
 require_once './vendor/autoload.php';
 
